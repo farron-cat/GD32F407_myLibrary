31f8243c246b3e97d69b7709d02660d61059585f
farron_cat <1627173301@qq.com>
2026-09-16 17:06:58 +0800

    [更新]: 测试串口

--- User/main.c
+++ User/main.c
@@ -40,12 +40,80 @@ OF SUCH DAMAGE.
 #include "bsp_keys.h"
 #include "bsp_leds.h"
 
-/*!
-    \brief    main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
+static void USART_config()
+{
+    /************** gpio config **************/
+    // 配置时钟
+    rcu_periph_clock_enable(RCU_GPIOA);
+    // 配置模式
+    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_9);
+    gpio_mode_set(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_PIN_10);
+    // 配置复用功能
+    gpio_af_set(GPIOA, GPIO_AF_7, GPIO_PIN_9);
+    gpio_af_set(GPIOA, GPIO_AF_7, GPIO_PIN_10);
+    // 配置输出参数
+    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
+    gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
+
+    /************** usart config **************/
+    // 串口时钟
+    rcu_periph_clock_enable(RCU_USART0);
+    // USART复位
+    usart_deinit(USART0);
+    // 波特率
+    usart_baudrate_set(USART0, 115200);
+    // 校验位
+    usart_parity_config(USART0, USART_PM_NONE);
+    // 数据位数
+    usart_word_length_set(USART0, USART_WL_8BIT);
+    // 停止位
+    usart_stop_bit_set(USART0, USART_STB_1BIT);
+    // 先发送高位还是低位
+    usart_data_first_config(USART0, USART_MSBF_LSB);
+    // 发送功能配置
+    usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
+    // 使能串口
+    usart_enable(USART0);
+}
+
+// 发送一byte数据
+static void send_byte(uint8_t data)
+{
+    // 通过USART发送
+    usart_data_transmit(USART0, data);
+    // 判断缓冲区是否已经空了
+    // FlagStatus state = usart_flag_get(USART_NUM,USART_FLAG_TBE);
+    while (RESET == usart_flag_get(USART0, USART_FLAG_TBE))
+        ;
+}
+
+// 发送多个byte数据
+void send_data_array(uint8_t *data, uint32_t len)
+{
+    while (data && len--)
+    {
+        send_byte(*data);
+        data++;
+    }
+}
+
+// 发送字符串
+static void send_string(char *data)
+{
+    // 满足: 1.data指针不为空  2.发送的数据不是\0结束标记
+    while (data && *data)
+    {
+        send_byte((uint8_t)(*data));
+        data++;
+    }
+}
+
+int fputc(int ch, FILE *f)
+{
+    send_byte((uint8_t)ch);
+    return ch;
+}
+
 int main(void)
 {
 
@@ -53,69 +121,68 @@ int main(void)
     bsp_leds_config();
     bsp_keys_config();
 
+    USART_config();
+
+    uint8_t cnt = 0U;
+
     while (1)
     {
 
-        // bsp_leds_open_all();
-        // delay_1ms(1000);
-        // bsp_leds_close_all();
-        // delay_1ms(1000);
+        // // KEY1 按下/松开控制 LED1
+        // switch (bsp_keys_scan(KEY1))
+        // {
+        // case KEY_EVENT_PRESSED:
+        //     bsp_leds_open(LED1);
+        //     break;
+        // case KEY_EVENT_RELEASED:
+        //     bsp_leds_close(LED1);
+        //     break;
+        // default:
+        //     break;
+        // }
 
-        // bsp_leds_open(LED2);
-        // delay_1ms(1000);
-        // bsp_leds_close(LED2);
-        // delay_1ms(1000);
+        // // KEY2 按下/松开控制 LED2
+        // switch (bsp_keys_scan(KEY2))
+        // {
+        // case KEY_EVENT_PRESSED:
+        //     bsp_leds_open(LED2);
+        //     break;
+        // case KEY_EVENT_RELEASED:
+        //     bsp_leds_close(LED2);
+        //     break;
+        // default:
+        //     break;
+        // }
 
-        // KEY1 按下/松开控制 LED1
-        switch (bsp_keys_scan(KEY1))
-        {
-        case KEY_EVENT_PRESSED:
-            bsp_leds_open(LED1);
-            break;
-        case KEY_EVENT_RELEASED:
-            bsp_leds_close(LED1);
-            break;
-        default:
-            break;
-        }
+        // // KEY3 按下/松开控制 LED3
+        // switch (bsp_keys_scan(KEY3))
+        // {
+        // case KEY_EVENT_PRESSED:
+        //     bsp_leds_open(LED3);
+        //     break;
+        // case KEY_EVENT_RELEASED:
+        //     bsp_leds_close(LED3);
+        //     break;
+        // default:
+        //     break;
+        // }
 
-        // KEY2 按下/松开控制 LED2
-        switch (bsp_keys_scan(KEY2))
-        {
-        case KEY_EVENT_PRESSED:
-            bsp_leds_open(LED2);
-            break;
-        case KEY_EVENT_RELEASED:
-            bsp_leds_close(LED2);
-            break;
-        default:
-            break;
-        }
+        // // KEY4 按下/松开控制 LED4
+        // switch (bsp_keys_scan(KEY4))
+        // {
+        // case KEY_EVENT_PRESSED:
+        //     bsp_leds_open(LED4);
+        //     break;
+        // case KEY_EVENT_RELEASED:
+        //     bsp_leds_close(LED4);
+        //     break;
+        // default:
+        //     break;
+        // }
 
-        // KEY3 按下/松开控制 LED3
-        switch (bsp_keys_scan(KEY3))
-        {
-        case KEY_EVENT_PRESSED:
-            bsp_leds_open(LED3);
-            break;
-        case KEY_EVENT_RELEASED:
-            bsp_leds_close(LED3);
-            break;
-        default:
-            break;
-        }
-
-        // KEY4 按下/松开控制 LED4
-        switch (bsp_keys_scan(KEY4))
-        {
-        case KEY_EVENT_PRESSED:
-            bsp_leds_open(LED4);
-            break;
-        case KEY_EVENT_RELEASED:
-            bsp_leds_close(LED4);
-            break;
-        default:
-            break;
-        }
+        // send_byte(cnt++);
+        // send_string("hello\r\n");
+        printf("hello %d\r\n", cnt++);
+        delay_1ms(10000);
     }
 }
